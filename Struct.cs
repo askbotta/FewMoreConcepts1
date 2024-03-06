e4d8a00d9a3520780532b3e540e8fa4bdb51fb40
Manoj <Manoj@DESKTOP-6S99LVD>
2024-03-06 23:59:52 +0530

    Add project files.

---
+++ Struct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FewMoreConcepts1
+{
+    public struct person
+    {
+        public string Name;
+        public int age;
+        public int weight;
+
+    }
+}
